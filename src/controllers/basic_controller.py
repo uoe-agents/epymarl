92075032c57e0351914cba2940e4465c02f49b27
micadam <aadam.michalski@gmail.com>
2022-08-01 21:12:30 +0100

    Remove avail_actions assertion
    Apparently parallel_runner doesn't like it.

--- src/controllers/basic_controller.py
+++ src/controllers/basic_controller.py
@@ -19,8 +19,6 @@ class BasicMAC:
     def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
         # Only select actions for the selected batch elements in bs
         avail_actions = ep_batch["avail_actions"][:, t_ep]
-        if not all(th.any(aa) for aa in avail_actions):
-            raise ValueError("All available actions must be non-empty")
         agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
         chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
         return chosen_actions
