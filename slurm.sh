6ba1e1b6a4322fee66cf153e3575c569ec7d2cff
Wira Azmoon <e0014172@u.nus.edu>
2023-03-13 12:14:17 +0800

    Add slurm script

---
+++ slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=epymarl
+#SBATCH --partition=long                        
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=65G                                     
+#SBATCH --time=24:00:00
+
+config=$1
+env_config=${2:-gymma}
+env_name=${3:-"pressureplate:pressureplate-linear-4p-v0"}
+
+python3 src/main.py --config=saf --env-config=gymma with env_args.time_limit=25 env_args.key="pressureplate:pressureplate-linear-4p-v0"
