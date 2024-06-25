5ba342f17891774e1c4549197294019958750e75
Lukas Schaefer <luki.schaefer96@gmail.com>
2024-06-25 14:26:15 +0100

    improve plotting script filtering and add option to plot best config per algorithm

--- plot_results.py
+++ plot_results.py
@@ -10,6 +10,8 @@ import seaborn as sns
 
 
 ALPHA = 0.2
+THRESHOLD_FOR_NUM_ALGS_UNTIL_LEGEND_BELOW_PLOT = 6
+THRESHOLD_FOR_ALG_NAME_LENGTH_UNTIL_LEGEND_BELOW_PLOT = 20
 
 
 def parse_args():
@@ -67,6 +69,11 @@ def parse_args():
         default=None,
         help="Smoothing window for data",
     )
+    parser.add_argument(
+        "--best_per_alg",
+        action="store_true",
+        help="Plot only best performing config per alg",
+    )
     return parser.parse_args()
 
 
@@ -88,7 +95,8 @@ def load_results(path, metric):
     path = Path(path)
     metrics_files = path.glob("**/metrics.json")
 
-    data = defaultdict(list)
+    # map (env_args, env_name, common_reward, reward_scalarisation) -> alg_name -> config-str -> (config, steps, values)
+    data = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
     for file in metrics_files:
         # load json
         with open(file, "r") as f:
@@ -116,7 +124,15 @@ def load_results(path, metric):
             continue
         del config["seed"]
 
-        data[str(config)].append((config, steps, values))
+        alg_name = extract_alg_name_from_config(config)
+        env_name = extract_env_name_from_config(config)
+        env_args = config["env_args"]
+        common_reward = config["common_reward"]
+        reward_scalarisation = config["reward_scalarisation"]
+
+        data[(str(env_args), env_name, common_reward, reward_scalarisation)][alg_name][
+            str(config)
+        ].append((config, steps, values))
     return data
 
 
@@ -129,28 +145,33 @@ def filter_results(data, filter_by_algs, filter_by_envs):
     :return: filtered data
     """
     filtered_data = data.copy()
-    delete_keys = set()
-    if filter_by_algs:
-        for key, results in data.items():
-            config = results[0][0]
-            alg_name = extract_alg_name_from_config(config)
-            if not any(alg in alg_name for alg in filter_by_algs):
-                delete_keys.add(key)
+
+    # filter envs
     if filter_by_envs:
-        for key, results in data.items():
-            config = results[0][0]
-            env_name = extract_env_name_from_config(config)
+        delete_env_keys = set()
+        for key in data:
+            env_name = key[1]
             if not any(env in env_name for env in filter_by_envs):
-                delete_keys.add(key)
-    for key in delete_keys:
-        del filtered_data[key]
+                delete_env_keys.add(key)
+        for key in delete_env_keys:
+            del filtered_data[key]
+
+    if filter_by_algs:
+        for env_key, env_data in filtered_data.items():
+            delete_alg_keys = set()
+            for alg_name in env_data:
+                if not any(alg in alg_name for alg in filter_by_algs):
+                    delete_alg_keys.add(alg_name)
+            for key in delete_alg_keys:
+                del filtered_data[env_key][key]
+
     return filtered_data
 
 
 def aggregate_results(data):
     """
     Aggregate results with mean and std over runs of the same config
-    :param data: dict with results
+    :param data: dict mapping key -> list of (config, steps, values)
     :return: aggregated data as dict with key -> (config, steps, means, stds)
     """
     agg_data = defaultdict(list)
@@ -206,27 +227,6 @@ def smooth_data(data, window_size):
     return data
 
 
-def group_data_by_task(data):
-    """
-    To generate one plot per task, group data by task
-    :param data: dict with results as dict of key -> (config, steps, means, stds)
-    :return: grouped data as dict of (env_args, env_name, common_reward, reward_scalarisation) ->
-        dict with (alg_name, key) -> (config, steps, means, stds)
-    """
-    grouped_data = defaultdict(dict)
-    for key, results in data.items():
-        config, steps, means, stds = results
-        alg_name = extract_alg_name_from_config(config)
-        env_name = extract_env_name_from_config(config)
-        env_args = config["env_args"]
-        common_reward = config["common_reward"]
-        reward_scalarisation = config["reward_scalarisation"]
-        grouped_data[(str(env_args), env_name, common_reward, reward_scalarisation)][
-            (alg_name, key)
-        ] = (config, steps, means, stds)
-    return grouped_data
-
-
 def _get_unique_keys(dicts):
     """
     Get all keys from a list of dicts that do not have identical values across all dicts
@@ -255,63 +255,85 @@ def _get_unique_keys(dicts):
     return unique_keys
 
 
-def get_data_with_unique_names(data):
+def shorten_config_names(data):
     """
-    Expand alg names for plot legend with all hyperparameters from config that are not identical
-        across all configs of that task
-    :param data: dict with results as dict with (alg_name, key) -> (config, steps, means, stds)
-    :return: dict with (expanded_alg_name) -> (config, steps, means, stds)
+    Shorten config names of algorithm to only include hyperparam values that differ across configs
+    :param data: dict with results as dict with config_str -> (config, steps, means, stds)
+    :return: dict with shortened_config_str -> (config, steps, means, stds)
     """
-    configs_by_alg_name = defaultdict(list)
-    for (alg_name, _), (config, _, _, _) in data.items():
-        configs_by_alg_name[alg_name].append(config)
+    configs = [config for config, _, _, _ in data.values()]
+    unique_keys_across_configs = _get_unique_keys(configs)
 
-    unique_keys_by_alg_name = {
-        alg_name: _get_unique_keys(configs)
-        for alg_name, configs in configs_by_alg_name.items()
-    }
-
-    expanded_data = {}
-    for (alg_name, _), (config, steps, means, stds) in data.items():
-        expanded_alg_name = alg_name
-        unique_keys_for_alg = unique_keys_by_alg_name[alg_name]
-        for key in unique_keys_for_alg:
+    shortened_data = {}
+    for config, steps, means, stds in data.values():
+        key_names = []
+        for key in unique_keys_across_configs:
             if key not in config:
                 continue
             value = config[key]
             if isinstance(value, float):
                 value = round(value, 4)
-            expanded_alg_name += f"_{key}={config[key]}"
-        expanded_data[expanded_alg_name] = (steps, means, stds)
-    return expanded_data
+            key_names.append(f"{key}={config[key]}")
+        shortened_config_name = "_".join(key_names)
+        shortened_data[shortened_config_name] = (config, steps, means, stds)
+    return shortened_data
 
 
 def _sorted_alg_names_by_mean(data):
     """
     Sort alg names by mean value of metric
-    :param data: dict with alg names -> (steps, means, stds)
+    :param data: dict with alg names -> (config, steps, means, stds)
     :return: list of sorted alg names
     """
-    return sorted(data, key=lambda x: np.mean(data[x][1]), reverse=True)
+    return sorted(data, key=lambda x: np.mean(data[x][2]), reverse=True)
 
 
-def plot_results(grouped_data, metric, save_dir, y_min, y_max, log_scale):
+def _filter_best_per_alg(data):
+    """
+    Filter data to only contain best performing config per alg
+    :param data: dict with key -> (config, steps, means, stds)
+    :return: key with highest mean value of means
+    """
+    means = {key: np.mean(data[key][2]) for key in data}
+    return max(means, key=means.get)
+
+
+def plot_results(data, metric, save_dir, y_min, y_max, log_scale):
     if save_dir is not None:
         save_dir.mkdir(parents=True, exist_ok=True)
     sns.set_style("whitegrid")
 
-    for (_, env, cr, rs), data in grouped_data.items():
+    for (_, env, cr, rs), env_data in data.items():
         plt.figure()
-        alg_names_by_performance = _sorted_alg_names_by_mean(data)
-        for alg_name in alg_names_by_performance:
-            steps, means, stds = data[alg_name]
-            plt.plot(steps, means, label=alg_name)
-            plt.fill_between(steps, means - stds, means + stds, alpha=ALPHA)
+        num_plots = 0
+        max_label_len = 0
+        for alg_name, alg_data in env_data.items():
+            if len(alg_data) == 1:
+                # plot single curve for algorithm
+                key = list(alg_data.keys())[0]
+                _, steps, means, stds = alg_data[key]
+                plt.plot(steps, means, label=alg_name)
+                plt.fill_between(steps, means - stds, means + stds, alpha=ALPHA)
+                num_plots += 1
+                max_label_len = max(max_label_len, len(alg_name))
+            else:
+                # plot multiple curves for algorithm, sorted by mean of means
+                config_keys_by_performance = _sorted_alg_names_by_mean(alg_data)
+                for config_key in config_keys_by_performance:
+                    _, steps, means, stds = alg_data[config_key]
+                    label = f"{alg_name} ({config_key})"
+                    plt.plot(steps, means, label=label)
+                    plt.fill_between(steps, means - stds, means + stds, alpha=ALPHA)
+                    num_plots += 1
+                    max_label_len = max(max_label_len, len(label))
         plt.title(f"Common Reward: {cr} (scalarisation: {rs})")
         plt.xlabel("Timesteps")
         plt.ylabel(metric)
 
-        if len(data) > 6:
+        if (
+            num_plots > THRESHOLD_FOR_NUM_ALGS_UNTIL_LEGEND_BELOW_PLOT
+            or max_label_len > THRESHOLD_FOR_ALG_NAME_LENGTH_UNTIL_LEGEND_BELOW_PLOT
+        ):
             # place legend below plot if there are many algos
             plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.15), ncol=3)
         else:
@@ -329,15 +351,39 @@ def main():
     args = parse_args()
     data = load_results(args.path, args.metric)
     data = filter_results(data, args.filter_by_algs, args.filter_by_envs)
-    data = aggregate_results(data)
-    if args.smoothing_window is not None:
-        data = smooth_data(data, args.smoothing_window)
-    grouped_data = group_data_by_task(data)
-    grouped_data_with_unique_names = {
-        key: get_data_with_unique_names(data) for key, data in grouped_data.items()
+    data = {
+        env_key: {
+            alg_name: aggregate_results(alg_data)
+            for alg_name, alg_data in env_data.items()
+        }
+        for env_key, env_data in data.items()
     }
+    if args.smoothing_window is not None:
+        data = {
+            env_key: {
+                alg_name: smooth_data(alg_data, args.smoothing_window)
+                for alg_name, alg_data in env_data.items()
+            }
+            for env_key, env_data in data.items()
+        }
+    data = {
+        env_key: {
+            alg_name: shorten_config_names(alg_data)
+            for alg_name, alg_data in env_data.items()
+        }
+        for env_key, env_data in data.items()
+    }
+    if args.best_per_alg:
+        best_data = defaultdict(dict)
+        for env_key, env_data in data.items():
+            for alg_name, alg_data in env_data.items():
+                best_config_key = _filter_best_per_alg(alg_data)
+                best_data[env_key][alg_name] = {
+                    best_config_key: alg_data[best_config_key]
+                }
+        data = best_data
     plot_results(
-        grouped_data_with_unique_names,
+        data,
         args.metric,
         Path(args.save_dir),
         args.y_min,
